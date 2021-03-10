d83e2f8ae8765e6b6983db1920205153f04ba257
Weslley Nascimento <weslley.nascimento@ice.ufjf.br>
2021-03-10 03:36:29 -0300

    adiciona passo a cena e sprite

--- js/Cena.js
+++ js/Cena.js
@@ -20,4 +20,9 @@ export default class Cena {
     adicionar(sprite) {
         this.sprites.push(sprite);
     }
+    passo(dt){
+        for (const sprite of this.sprites) {
+            sprite.passo(dt);
+        }
+    }
 }
