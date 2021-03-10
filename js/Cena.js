da23551ff177ed1dc8d04503412f6f49defeae73
Weslley Nascimento <weslley.nascimento@ice.ufjf.br>
2021-03-10 03:51:52 -0300

    adiciona teste de colisao

--- js/Cena.js
+++ js/Cena.js
@@ -23,28 +23,43 @@ export default class Cena {
     adicionar(sprite) {
         this.sprites.push(sprite);
     }
-    passo(dt){
+    passo(dt) {
         for (const sprite of this.sprites) {
             sprite.passo(dt);
         }
     }
 
-    quadro(t){
+    quadro(t) {
         this.t0 = this.t0 ?? t;
-        this.dt = (t - this.t0)/1000;
+        this.dt = (t - this.t0) / 1000;
 
         this.passo(this.dt);
         this.desenhar();
+        this.checaColisao();
 
         this.iniciar();
         this.t0 = t;
     }
-    iniciar(){
-        this.idAnim = requestAnimationFrame((t) => {this.quadro(t);});
+
+    iniciar() {
+        this.idAnim = requestAnimationFrame((t) => { this.quadro(t); });
     }
+
     parar() {
         cancelAnimationFrame(this.idAnim);
         this.t0 = null;
         this.dt = 0;
+    }
+
+    checaColisao() {
+        for (let a = 0; a < this.sprites.length - 1; a++) {
+            const spriteA = this.sprites[a];
+            for (let b = a + 1; b < this.sprites.length; b++) {
+                const spriteB = this.sprites[b];
+                if (spriteA.colidiuCom(spriteB)) {
+                    console.log(spriteA, spriteB);
+                }
+            }
+        }
     }
 }
