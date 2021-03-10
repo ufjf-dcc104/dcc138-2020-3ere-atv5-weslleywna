76cce9396a848c4318c665df864c89e928a8f0ff
Weslley Nascimento <weslley.nascimento@ice.ufjf.br>
2021-03-10 04:21:31 -0300

    refatora carregamento e uso de imagem para o asset manager

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -1,12 +1,24 @@
-export default class AssetManager{
+export default class AssetManager {
     constructor() {
         this.aCarregar = 0;
         this.carregadas = 0;
+        this.imagens = new Map();
     }
 
-    progresso () {
+    carregaImagem(chave, source) {
+        const img1 = new Image();
+        img1.src = source;
+
+        this.imagens.set(chave, img1);
+    }
+
+    img(chave) {
+        return this.imagens.get(chave);
+    }
+
+    progresso() {
         if (this.aCarregar > 0) {
-            return `${(this.carregadas/this.aCarregar*100).toFixed(2)}%`;
+            return `${(this.carregadas / this.aCarregar * 100).toFixed(2)}%`;
         }
         return "Nada a carregar";
     }
