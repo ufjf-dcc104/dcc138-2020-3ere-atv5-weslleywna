752651c76a7bcd07fb122d5f32165aeb47cdb963
Weslley Nascimento <weslley.nascimento@ice.ufjf.br>
2021-03-10 03:58:48 -0300

    adiciona remocao de sprites

--- js/Cena.js
+++ js/Cena.js
@@ -37,6 +37,7 @@ export default class Cena {
         this.passo(this.dt);
         this.desenhar();
         this.checaColisao();
+        this.removerSprites();
 
         this.iniciar();
         this.t0 = t;
@@ -71,6 +72,14 @@ export default class Cena {
         if (!this.aRemover.includes(b)) {
             this.aRemover.push(b);
         }
-        console.log(this.aRemover);
+    }
+
+    removerSprites() {
+        for (const alvo of this.aRemover) {
+            const idx = this.sprites.indexOf(alvo);
+            if (idx >= 0) {
+                this.sprites.splice(idx, 1);
+            }
+        }
     }
 }
