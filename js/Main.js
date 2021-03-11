9e399888f2322c82faa52055663b8963372b384c
Weslley Nascimento <weslley.nascimento@ice.ufjf.br>
2021-03-11 15:34:10 -0300

    cria um mapa para uso na cena

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,6 @@
 import AssetManager from "./AssetManager.js";
 import Cena from "./Cena.js";
+import Mapa from "./Mapa.js";
 import Mixer from "./Mixer.js";
 import Sprite from "./Sprite.js";
 
@@ -12,12 +13,18 @@ assets.carregaImagem("orc", "assets/orc.png");
 assets.carregaAudio("moeda", "assets/coin.wav");
 assets.carregaAudio("boom", "assets/boom.wav");
 
-document.body.appendChild(assets.img("garota"));
-document.body.appendChild(assets.img("esqueleto"));
-document.body.appendChild(assets.img("orc"));
+// document.body.appendChild(assets.img("garota"));
+// document.body.appendChild(assets.img("esqueleto"));
+// document.body.appendChild(assets.img("orc"));
 
 const canvas = document.querySelector("canvas");
+canvas.width = 14 * 32;
+canvas.height = 10 * 32;
 const cena1 = new Cena(canvas, assets);
+
+const mapa1 = new Mapa(10, 14, 32);
+cena1.configuraMapa(mapa1);
+
 const pc = new Sprite({ vx: 10 });
 const en1 = new Sprite({ x: 140, w: 30, color: "red" });
 
