11442827e23154a96424a221cd23b2f042330bd4
Weslley Nascimento <weslley.nascimento@ice.ufjf.br>
2021-03-11 15:53:54 -0300

    refatora sprite para centro de massa

--- js/Sprite.js
+++ js/Sprite.js
@@ -1,8 +1,8 @@
 export default class Sprite {
     /*
         Reponsavel por modelar algo que se move na tela.
-    */ 
-    constructor({x=100, y=100, w=20, h=20, color="white", vx=0,vy=0}={}) {
+    */
+    constructor({ x = 100, y = 100, w = 20, h = 20, color = "white", vx = 0, vy = 0 } = {}) {
         this.x = x;
         this.y = y;
         this.vx = vx;
@@ -13,19 +13,19 @@ export default class Sprite {
     }
     desenhar(ctx) {
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.w, this.h);
+        ctx.fillRect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
     }
-    passo(dt){
-        this.x = this.x + this.vx*dt;
-        this.y = this.y + this.vy*dt;
+    passo(dt) {
+        this.x = this.x + this.vx * dt;
+        this.y = this.y + this.vy * dt;
     }
 
     colidiuCom(outro) {
         return !(
-            this.x > outro.x + outro.w
-            || this.x +this.w < outro.x
-            || this.y > outro.y + outro.h
-            || this.y +this.y < outro.y
+            this.x - this.w / 2 > outro.x + outro.w / 2
+            || this.x + this.w / 2 < outro.x - outro.w / 2
+            || this.y - this.h / 2 > outro.y + outro.h / 2
+            || this.y + this.h / 2 < outro.y - outro.h / 2
         );
     }
 }
