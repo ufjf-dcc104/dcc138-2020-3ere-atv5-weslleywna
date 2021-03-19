5f8e11c4eb780cd73b988f44e52069e79e3afeac
Weslley Nascimento <weslley.nascimento@ice.ufjf.br>
2021-03-19 12:50:38 -0300

    adiciona mais teclas para controle do pc

--- js/Main.js
+++ js/Main.js
@@ -28,6 +28,8 @@ input.configurarTeclado(
     {
         "ArrowLeft": "MOVE_ESQUERDA",
         "ArrowRight": "MOVE_DIREITA",
+        "ArrowUp": "MOVE_CIMA",
+        "ArrowDown": "MOVE_BAIXO",
     }
 );
 
@@ -45,6 +47,14 @@ pc.controlar = function(dt) {
         this.vx = 50;
     } else {
         this.vx = 0;
+    }
+
+    if (input.comandos.get("MOVE_CIMA")) {
+        this.vy = -50;
+    } else if (input.comandos.get("MOVE_BAIXO")) {
+        this.vy = 50;
+    } else {
+        this.vy = 0;
     }
 };
 const en1 = new Sprite({ x: 160, vx: -10, color: "red" });
